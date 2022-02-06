112a91b92b46dd1745c8c42dbe4de0a182abd793
Alessandro <alessandro01desideri@gmail.com>
2022-02-06 17:40:45 +0100

    Completamento esercizio

---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="title">
+    Prima app con Vue CLI
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+}
+</script>
+
+<style scoped lang="scss">
+.title {
+  font-size: 35px;
+  font-weight: 600;
+}
+</style>
